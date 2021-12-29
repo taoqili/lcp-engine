54552f66d8d1cab3be7efe82056309cb1c638fff
taoqili <taoqili@kuaishou.com>
2021-12-29 17:51:41 +0800

    lcp-engine: 生产环境开启css压缩

--- packages/lcp-engine/scripts/webpack.prod.js
+++ packages/lcp-engine/scripts/webpack.prod.js
@@ -2,6 +2,7 @@ const { merge } = require('webpack-merge')
 const common = require('./webpack.common')
 const { OUTPUT_DIR } = require('./constant')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 
 module.exports = merge(common, {
   mode: 'production',
@@ -15,5 +16,11 @@ module.exports = merge(common, {
       filename: 'static/[name].[contenthash:8].css',
       chunkFilename: 'static/[name].[contenthash:8].chunk.css',
     }),
-  ]
+  ],
+  optimization: {
+    minimize: true,
+    minimizer:[
+      new CssMinimizerPlugin()
+    ]
+  }
 })
